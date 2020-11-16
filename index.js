9131330101aa218984fc39f6429c2962db006d1d
Gentlesammy <samodunlade84@gmail.com>
2020-11-16 21:26:51 +0100

    env file configured

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { SECRET, DB_LINK, PORT } = require("./config/index");
 
-const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect(
+  DB_LINK,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("connected");
+  }
+);
+
 app.listen(PORT, () => {
   console.log("connected");
 });
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.send("hello world");
 });

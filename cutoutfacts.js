46369659089c9941a25741a163841d4ac8b380df
Gentlesammy <samodunlade84@gmail.com>
2020-11-26 15:56:36 +0100

    packageJson error fixed

---
+++ cutoutfacts.js
@@ -0,0 +1,8 @@
+/*
+
+    "server": "nodemon index",
+    "client": "npm run start --prefix  client",
+    "dev": "concurrently \"npm run server \"  \"npm run client\"",
+    "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false  npm install --prefix client && npm run build --prefix client"
+
+*/
